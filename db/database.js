f6c1f21ed7ab8f5552ffa7db4bfd8935d76f4971
jorgeebn16 <jorgeebn16@gmail.com>
2021-02-01 21:21:04 -0500

    Database Update
    Database Update

---
+++ db/database.js
@@ -0,0 +1,137 @@
+const db = require('./dbconnect');
+
+const database = {
+    dropEmployeeTable: 'DROP TABLE IF EXISTS employee',
+    dropRoleTable: 'DROP TABLE IF EXISTS role',
+    dropDepartmentTable: 'DROP TABLE IF EXISTS department',
+    createDepartmentTable: `CREATE TABLE IF NOT EXISTS department (
+      id INT AUTO_INCREMENT,
+      name VARCHAR(30) NOT NULL,
+      PRIMARY KEY(id)
+    )`,
+    createRoleTable: `CREATE TABLE IF NOT EXISTS role (
+      id INT AUTO_INCREMENT,
+      title VARCHAR(30) NOT NULL,
+      salary DECIMAL NOT NULL,
+      department_id INT NOT NULL,
+      PRIMARY KEY(id),
+      FOREIGN KEY(department_id) REFERENCES department(id)
+    )`,
+    createEmployeeTable: `CREATE TABLE IF NOT EXISTS employee(
+      id INT AUTO_INCREMENT,
+      first_name VARCHAR(30) NOT NULL,
+      last_name VARCHAR(30) NOT NULL,
+      role_id INT NOT NULL,
+      manager_id INT,
+      PRIMARY KEY(id),
+      FOREIGN KEY(role_id) REFERENCES role(id),
+      FOREIGN KEY(manager_id) REFERENCES employee(id) 
+    )`,
+
+    reset: function() {
+      db.query(this.dropEmployeeTable, err => {
+        if (err) throw err;
+      });
+  
+      db.query(this.dropRoleTable, err => {
+        if (err) throw err;
+      });
+  
+      db.query(this.dropDepartmentTable, err => {
+        if (err) throw err;
+      });
+  
+      db.query(this.createDepartmentTable, err => {
+        if (err) throw err;
+      });
+  
+      db.query(this.createRoleTable, err => {
+        if (err) throw err;
+      });
+  
+      db.query(this.createEmployeeTable, err => {
+        if (err) throw err;
+      });
+    },
+    init: function() {
+      // Seed department table
+      db.query(
+        `INSERT INTO department (name) 
+          VALUES 
+            ('Sales'),
+            ('Legal'),
+            ('Fianace'),
+            ('Engineering')`,
+        err => {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+        }
+      );
+  
+      // Seed role table
+      db.query(
+        `INSERT INTO role (title, salary, department_id) 
+          VALUES 
+            ('Sales Lead', 100000, 1),
+            ('Salesperson', 80000, 1),
+            ('Lawyer', 190000, 2),
+            ('Legal Team Lead', 250000, 2),
+            ('Accountant', 125000, 3),
+            ('Software Engineer', 120000, 4),
+            ('Lead Software Engineer', 180000, 4)`,
+        err => {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+        }
+      );
+  
+      // Seed employees
+      db.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+          VALUES 
+            ('Jorge', 'Navarro', 7, null),
+            ('Lina', 'Viveros', 4, null),
+            ('Clio', 'Gordon', 1, null),
+            ('Sean', 'Arians', 5, null),
+            ('Theo', 'Macias', 3, 4),
+            ('Patricia', 'Brady', 2, 1),
+            ('Enrique', 'Payton', 6, 7),
+            ('Ana', 'Solo', 2, 1)`,
+        err => {
+          if (err) {
+            console.log(err);
+            throw err;
+          }
+        }
+      );
+    },
+    setupDB: function(){
+      this.reset();
+      this.init();
+    },
+    dropAndEnd: function(){
+
+        db.query(this.dropEmployeeTable, (err, result) => {
+            if (err) throw err;
+            console.log(this.dropEmployeeTable);
+          });
+      
+          db.query(this.dropRoleTable, (err, result) => {
+            if (err) throw err;
+            console.log(this.dropRoleTable);
+          });
+      
+          db.query(this.dropDepartmentTable, (err, result) => {
+            if (err) throw err;
+            console.log(this.dropDepartmentTable);
+          });
+      
+          db.end();
+        }
+  };
+  
+module.exports = database;
---
+++ utils/logging.js
@@ -0,0 +1,33 @@
+function displayHeadline(str) {
+  let headline = "\n";
+  headline += "------------------------------    ";
+  headline += str.toUpperCase();
+  headline += "    ------------------------------";
+  headline += "\n";
+  
+  console.log(headline);
+  return headline;
+}
+
+function displayResults(result) {
+  console.table(result);
+
+}
+
+function displayFooter(headline) {
+  const length = headline.length;
+  let footer = "-";
+  for (let i = 0; i < length; i++) {
+    footer += "-";
+  }
+  footer += "\n";
+
+  console.log(footer);
+
+}
+
+module.exports = {
+  displayHeadline,
+  displayFooter,
+  displayResults,
+};
